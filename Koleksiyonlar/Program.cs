bf7f566e231b3ac3a0c9d77ded02cde7f4a6346e
Idris Uslu <idrisuslu.007@hotmail.com>
2021-03-12 00:17:55 +0300

    değiştirildi

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Koleksiyonlar
 {
@@ -6,7 +7,17 @@ namespace Koleksiyonlar
     {
         static void Main(string[] args)
         {
+            List<string> isimler = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
 
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+
+            isimler.Add("İlker");
+            
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
         }
     }
 }
